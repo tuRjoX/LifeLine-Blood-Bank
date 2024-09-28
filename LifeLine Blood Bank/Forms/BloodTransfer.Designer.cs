cd8eb88c693ba2f617fa39d4f224f496798bd85f
Saif Ahmed <110305537+SaifAhmed059@users.noreply.github.com>
2024-09-28 21:48:33 +0600

    .

--- LifeLine Blood Bank/Forms/BloodTransfer.Designer.cs
+++ LifeLine Blood Bank/Forms/BloodTransfer.Designer.cs
@@ -45,11 +45,11 @@
             this.BloodGroup.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.BloodGroup.Enabled = false;
             this.BloodGroup.Font = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BloodGroup.Location = new System.Drawing.Point(660, 298);
-            this.BloodGroup.Margin = new System.Windows.Forms.Padding(2);
+            this.BloodGroup.Location = new System.Drawing.Point(880, 364);
+            this.BloodGroup.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BloodGroup.Multiline = true;
             this.BloodGroup.Name = "BloodGroup";
-            this.BloodGroup.Size = new System.Drawing.Size(163, 24);
+            this.BloodGroup.Size = new System.Drawing.Size(217, 30);
             this.BloodGroup.TabIndex = 51;
             // 
             // label5
@@ -57,10 +57,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(335, 35);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(447, 43);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(252, 38);
+            this.label5.Size = new System.Drawing.Size(311, 47);
             this.label5.TabIndex = 50;
             this.label5.Text = "Blood Transfer";
             // 
@@ -70,9 +69,10 @@
             this.TransferBtn.FlatAppearance.BorderSize = 0;
             this.TransferBtn.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TransferBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
-            this.TransferBtn.Location = new System.Drawing.Point(389, 470);
+            this.TransferBtn.Location = new System.Drawing.Point(519, 578);
+            this.TransferBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TransferBtn.Name = "TransferBtn";
-            this.TransferBtn.Size = new System.Drawing.Size(117, 45);
+            this.TransferBtn.Size = new System.Drawing.Size(156, 55);
             this.TransferBtn.TabIndex = 52;
             this.TransferBtn.Text = "Transfer";
             this.TransferBtn.UseVisualStyleBackColor = false;
@@ -84,12 +84,11 @@
             this.AvailableLbl.AutoSize = true;
             this.AvailableLbl.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AvailableLbl.ForeColor = System.Drawing.Color.Red;
-            this.AvailableLbl.Location = new System.Drawing.Point(368, 420);
-            this.AvailableLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.AvailableLbl.Location = new System.Drawing.Point(491, 517);
             this.AvailableLbl.Name = "AvailableLbl";
-            this.AvailableLbl.Size = new System.Drawing.Size(164, 23);
+            this.AvailableLbl.Size = new System.Drawing.Size(218, 28);
             this.AvailableLbl.TabIndex = 49;
-            this.AvailableLbl.Text = "AvailableOrNot";
+            this.AvailableLbl.Text = "Available Or Not";
             this.AvailableLbl.Visible = false;
             // 
             // PatNameTb
@@ -98,11 +97,11 @@
             this.PatNameTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.PatNameTb.Enabled = false;
             this.PatNameTb.Font = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PatNameTb.Location = new System.Drawing.Point(369, 298);
-            this.PatNameTb.Margin = new System.Windows.Forms.Padding(2);
+            this.PatNameTb.Location = new System.Drawing.Point(492, 364);
+            this.PatNameTb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.PatNameTb.Multiline = true;
             this.PatNameTb.Name = "PatNameTb";
-            this.PatNameTb.Size = new System.Drawing.Size(163, 24);
+            this.PatNameTb.Size = new System.Drawing.Size(217, 30);
             this.PatNameTb.TabIndex = 48;
             // 
             // label3
@@ -110,10 +109,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(365, 243);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(487, 299);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.Size = new System.Drawing.Size(151, 23);
             this.label3.TabIndex = 47;
             this.label3.Text = "Patient Name";
             // 
@@ -122,10 +120,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(656, 243);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(875, 299);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.Size = new System.Drawing.Size(136, 23);
             this.label2.TabIndex = 46;
             this.label2.Text = "Blood Group";
             // 
@@ -135,10 +132,10 @@
             this.PatientIdCb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.PatientIdCb.Font = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PatientIdCb.FormattingEnabled = true;
-            this.PatientIdCb.Location = new System.Drawing.Point(102, 301);
-            this.PatientIdCb.Margin = new System.Windows.Forms.Padding(2);
+            this.PatientIdCb.Location = new System.Drawing.Point(136, 367);
+            this.PatientIdCb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.PatientIdCb.Name = "PatientIdCb";
-            this.PatientIdCb.Size = new System.Drawing.Size(163, 24);
+            this.PatientIdCb.Size = new System.Drawing.Size(216, 28);
             this.PatientIdCb.TabIndex = 45;
             this.PatientIdCb.SelectedIndexChanged += new System.EventHandler(this.PatientIdCb_SelectedIndexChanged);
             // 
@@ -147,18 +144,17 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(99, 243);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(132, 299);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.Size = new System.Drawing.Size(118, 23);
             this.label1.TabIndex = 44;
             this.label1.Text = "Patient ID";
             // 
             // BloodTransfer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(898, 619);
+            this.ClientSize = new System.Drawing.Size(1197, 762);
             this.Controls.Add(this.BloodGroup);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.TransferBtn);
@@ -168,6 +164,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.PatientIdCb);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "BloodTransfer";
             this.Text = "BloodTransfer";
             this.Load += new System.EventHandler(this.BloodTransfer_Load);
