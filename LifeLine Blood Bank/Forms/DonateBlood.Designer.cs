4f9d4093fd79039b2dcf0c7579fa9aa023233a28
Turjo Das Dip <tdas41380@gmail.com>
2024-09-29 00:38:42 +0600

    .

--- LifeLine Blood Bank/Forms/DonateBlood.Designer.cs
+++ LifeLine Blood Bank/Forms/DonateBlood.Designer.cs
@@ -60,6 +60,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
