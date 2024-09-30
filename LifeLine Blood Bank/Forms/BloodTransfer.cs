65dddff275a07e84786e0acdd193be5c02a439bc
Turjo Das Dip <tdas41380@gmail.com>
2024-09-30 09:20:17 +0600

    Changed

--- LifeLine Blood Bank/Forms/BloodTransfer.cs
+++ LifeLine Blood Bank/Forms/BloodTransfer.cs
@@ -1,94 +1,67 @@
 ﻿using System;
 using System.Data;
-using System.Data.SqlClient;
 using System.Windows.Forms;
-using System.Configuration;
 using System.Drawing;
+using LifeLineBloodBank.Database;
 
 namespace LifeLineBloodBank.Forms
 {
     public partial class BloodTransfer : Form
     {
+        private readonly PatientsTbl patientsTbl;
+        private readonly BloodTbl bloodTbl;
+        private readonly TransferTbl transferTbl;
+
         public BloodTransfer()
         {
             InitializeComponent();
+            patientsTbl = new PatientsTbl();
+            bloodTbl = new BloodTbl();
+            transferTbl = new TransferTbl();
             fillPatientCb();
         }
+
         private void LoadTheme()
         {
             foreach (Control btns in this.Controls)
             {
-                if (btns.GetType() == typeof(Button))
+                if (btns is Button btn)
                 {
-                    Button btn = (Button)btns;
                     btn.BackColor = ThemeColor.PrimaryColor;
-                    btn.ForeColor = Color.Honeydew;
                     btn.ForeColor = Color.White;
                     btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
                 }
-
-                label1.ForeColor = ThemeColor.SecondaryColor;
-                label2.ForeColor = ThemeColor.PrimaryColor;
-                label3.ForeColor = ThemeColor.SecondaryColor;
-                label5.ForeColor = ThemeColor.PrimaryColor;
-                AvailableLbl.ForeColor = ThemeColor.SecondaryColor;
             }
+
+            label1.ForeColor = ThemeColor.SecondaryColor;
+            label2.ForeColor = ThemeColor.PrimaryColor;
+            label3.ForeColor = ThemeColor.SecondaryColor;
+            label5.ForeColor = ThemeColor.PrimaryColor;
+            AvailableLbl.ForeColor = ThemeColor.SecondaryColor;
         }
+
         private void fillPatientCb()
         {
-            using (SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString))
-            {
-                Con.Open();
-                SqlCommand cmd = new SqlCommand("select PNum from PatientsTbl", Con);
-                SqlDataReader rdr = cmd.ExecuteReader();
-                DataTable dt = new DataTable();
-                dt.Columns.Add("PNum", typeof(int));
-                dt.Load(rdr);
-                PatientIdCb.ValueMember = "PNum";
-                PatientIdCb.DataSource = dt;
-            }
+            DataTable dt = patientsTbl.GetPatients();
+            PatientIdCb.ValueMember = "PNum";
+            PatientIdCb.DataSource = dt;
         }
 
         private void GetData()
         {
-            using (SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString))
+            int patientId = (int)PatientIdCb.SelectedValue;
+            DataTable dt = patientsTbl.GetPatientById(patientId);
+            if (dt.Rows.Count > 0)
             {
-                Con.Open();
-                string query = "select * from PatientsTbl where PNum=" + PatientIdCb.SelectedValue.ToString() + "";
-                SqlCommand cmd = new SqlCommand(query, Con);
-                DataTable dt = new DataTable();
-                SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                sda.Fill(dt);
-                foreach (DataRow dr in dt.Rows)
-                {
-                    PatNameTb.Text = dr["PName"].ToString();
-                    BloodGroup.Text = dr["PBGroup"].ToString();
-                }
-            }
-        }
-
-        int stock = 0;
-        private void GetStock(string Bgroup)
-        {
-            using (SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString))
-            {
-                Con.Open();
-                string query = "select * from BloodTbl where BGroup='" + Bgroup + "'";
-                SqlCommand cmd = new SqlCommand(query, Con);
-                DataTable dt = new DataTable();
-                SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                sda.Fill(dt);
-                foreach (DataRow dr in dt.Rows)
-                {
-                    stock = Convert.ToInt32(dr["BStock"].ToString());
-                }
+                PatNameTb.Text = dt.Rows[0]["PName"].ToString();
+                BloodGroup.Text = dt.Rows[0]["PBGroup"].ToString();
             }
         }
 
         private void PatientIdCb_SelectedIndexChanged(object sender, EventArgs e)
         {
             GetData();
-            GetStock(BloodGroup.Text);
+            int stock = bloodTbl.GetStockByBloodGroup(BloodGroup.Text);
             if (stock > 0)
             {
                 TransferBtn.Visible = true;
@@ -111,50 +84,25 @@ namespace LifeLineBloodBank.Forms
             TransferBtn.Visible = false;
         }
 
-        private void updateStock()
-        {
-            int newstock = stock - 1;
-            using (SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString))
-            {
-                try
-                {
-                    string query = "update BloodTbl set BStock=" + newstock + " where BGroup='" + BloodGroup.Text + "';";
-                    Con.Open();
-                    SqlCommand cmd = new SqlCommand(query, Con);
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception Ex)
-                {
-                    MessageBox.Show(Ex.Message);
-                }
-            }
-        }
-
         private void TransferBtn_Click(object sender, EventArgs e)
         {
-            if (PatNameTb.Text == "")
+            if (string.IsNullOrEmpty(PatNameTb.Text))
             {
                 MessageBox.Show("Missing Information");
             }
             else
             {
-                using (SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString))
+                try
                 {
-                    try
-                    {
-                        string query = "insert into TransferTbl values('" + PatNameTb.Text + "','" + BloodGroup.Text + "' )";
-                        Con.Open();
-                        SqlCommand cmd = new SqlCommand(query, Con);
-                        cmd.ExecuteNonQuery();
-                        MessageBox.Show("Successfully Transfer.");
-                        GetStock(BloodGroup.Text);
-                        updateStock();
-                        Reset();
-                    }
-                    catch (Exception Ex)
-                    {
-                        MessageBox.Show(Ex.Message);
-                    }
+                    transferTbl.AddTransfer(PatNameTb.Text, BloodGroup.Text);
+                    int newStock = bloodTbl.GetStockByBloodGroup(BloodGroup.Text) - 1;
+                    bloodTbl.UpdateStock(BloodGroup.Text, newStock);
+                    MessageBox.Show("Successfully Transferred.");
+                    Reset();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
                 }
             }
         }
